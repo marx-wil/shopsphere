67e072b9340c7f3b6e83acdcaedfc164d126cd06
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-03 10:53:32 +0800

    chore: script to import and export brand assets

---
+++ src/assets/brand/index.js
@@ -0,0 +1,6 @@
+import LogoColored from "./logo-color.png";
+import LogoGrayscale from "./logo-grayscale.png";
+import LogoGrayscaleInverted from "./logo-grayscale-inverted.png";
+import LogoNoBackground from "./logo-no-background.png";
+
+export { LogoColored, LogoGrayscale, LogoGrayscaleInverted, LogoNoBackground };
