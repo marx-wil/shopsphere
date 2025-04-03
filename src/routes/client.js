745953af8a976d5272705b3ece099d2f51b0dc4c
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-03 10:54:21 +0800

    chore: added client routes

---
+++ src/routes/client.js
@@ -0,0 +1,17 @@
+const ClientRoutes = [
+  {
+    path: "/",
+  },
+  {
+    path: "/cart",
+  },
+  {
+    path: "/checkout",
+  },
+  {
+    path: "/product-details/:id",
+  },
+  {
+    path: "/product-listing",
+  },
+];
