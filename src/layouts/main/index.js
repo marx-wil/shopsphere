065d637286f0b32fec7bc6f9d6b56262c55a4cb2
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-03 11:16:50 +0800

    chore: imported floating color mde button

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,5 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import NavigationBar from "../../components/navigationbar";
+import FloatingColorMode from "../../components/floatingColorMode";
 
 const Layout = (Component) => {
   const DefaultLayout = ({ ...props }) => {
@@ -10,6 +11,7 @@ const Layout = (Component) => {
           {/* Your wrapped component is rendered here with all its props */}
           <Component {...props} />
           <Text>Footer here</Text>
+          <FloatingColorMode />
         </Box>
       </>
     );
