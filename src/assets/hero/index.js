7eca04662a64f66df320114f2b3633dc6f08902b
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-04 09:52:36 +0800

    chore: import and export new hero images

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,3 +1,10 @@
 import MainHeroImage from "./front-view-parsnip.jpg";
-
-export { MainHeroImage };
+import GardenImage from "./gardening-concept-with-plants-apron.jpg";
+import HomeGardenArrangement from "./home-garden-arrangement-with-copy-space.jpg";
+import WhiteWoodBackgroundPlants from "./white-wooden-background-with-plants.jpg";
+export {
+  MainHeroImage,
+  GardenImage,
+  HomeGardenArrangement,
+  WhiteWoodBackgroundPlants,
+};
