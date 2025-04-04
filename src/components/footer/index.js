210c33bb90196ce4b1a25681f30e936c597bf4a3
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-04 11:16:01 +0800

    feat: footer design and layout

---
+++ src/components/footer/index.js
@@ -0,0 +1,160 @@
+"use client";
+
+import {
+  Box,
+  chakra,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FaInstagram, FaGithub, FaFacebook } from "react-icons/fa6";
+import { BiMailSend } from "react-icons/bi";
+
+import Brand from "../../components/navigationbar/components/brand";
+const Logo = () => {
+  return (
+    <Box>
+      <Brand />
+    </Box>
+  );
+};
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded="full"
+      w={8}
+      h={8}
+      cursor="pointer"
+      as="a"
+      href={href}
+      display="inline-flex"
+      alignItems="center"
+      justifyContent="center"
+      transition="background 0.3s ease"
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+          spacing={8}
+        >
+          <Stack spacing={6}>
+            <Box>
+              <Logo color={useColorModeValue("gray.700", "white")} />
+            </Box>
+            <Text fontSize={"sm"}>
+              © 2025 Wilmarx John Cayabyab. All rights reserved
+            </Text>
+            <Stack direction={"row"} spacing={6}>
+              <SocialButton
+                label={"Instagram"}
+                href={"https://www.instagram.com/err.marx/"}
+              >
+                <FaInstagram />
+              </SocialButton>
+              <SocialButton
+                label={"Github"}
+                href={"https://github.com/marx-wil"}
+              >
+                <FaGithub />
+              </SocialButton>
+              <SocialButton
+                label={"Facebook"}
+                href={"https://www.facebook.com/err.marx"}
+              >
+                <FaFacebook />
+              </SocialButton>
+            </Stack>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Box as="a" href={"#"}>
+              About us
+            </Box>
+            <Box as="a" href={"#"}>
+              Blog
+            </Box>
+            <Box as="a" href={"#"}>
+              Contact us
+            </Box>
+            <Box as="a" href={"#"}>
+              Pricing
+            </Box>
+            <Box as="a" href={"#"}>
+              Testimonials
+            </Box>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Support</ListHeader>
+            <Box as="a" href={"#"}>
+              Help Center
+            </Box>
+            <Box as="a" href={"#"}>
+              Terms of Service
+            </Box>
+            <Box as="a" href={"#"}>
+              Legal
+            </Box>
+            <Box as="a" href={"#"}>
+              Privacy Policy
+            </Box>
+            <Box as="a" href={"#"}>
+              Satus
+            </Box>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Stay up to date</ListHeader>
+            <Stack direction={"row"}>
+              <Input
+                placeholder={"Your email address"}
+                bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+                border={0}
+                _focus={{
+                  bg: "whiteAlpha.300",
+                }}
+              />
+              <IconButton
+                bg={useColorModeValue("green.400", "green.800")}
+                color={useColorModeValue("white", "gray.800")}
+                _hover={{
+                  bg: "green.600",
+                }}
+                aria-label="Subscribe"
+                icon={<BiMailSend />}
+              />
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
