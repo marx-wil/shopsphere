e5b8392e578e9729f6382bcdc3e998127dfa565d
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-04 09:55:38 +0800

    chore: import image as header

--- src/components/heroHeaderSection/index.js
+++ src/components/heroHeaderSection/index.js
@@ -1,4 +1,7 @@
-import { WhiteWoodBackgroundPlants } from "../../assets/hero/index";
+import {
+  MainHeroImage,
+  WhiteWoodBackgroundPlants,
+} from "../../assets/hero/index";
 import {
   Flex,
   Stack,
@@ -158,7 +161,7 @@ const HeroHeaderSection = () => {
             width={"full"}
             overflow={"hidden"}
           >
-            <IconButton
+            {/* <IconButton
               aria-label={"Play Button"}
               variant={"ghost"}
               _hover={{ bg: "transparent" }}
@@ -168,17 +171,15 @@ const HeroHeaderSection = () => {
               position={"absolute"}
               left={"50%"}
               top={"50%"}
-              transform={"translateX(-50%) translateY(-50%)"}
-            />
+              transform={"translateX(-50%) translateY(-50%)"} */}
+            {/* /> */}
             <Image
               alt={"Hero Image"}
               fit={"cover"}
               align={"center"}
               w={"100%"}
               h={"100%"}
-              src={
-                "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
-              }
+              src={MainHeroImage}
             />
           </Box>
         </Flex>
