b4c9486669f49eab13de29c9fbebeb9409a9e293
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-04 10:58:53 +0800

    chore: moved user button and cart button to a new component

--- src/components/navigationbar/components/actionButtons/index.js
+++ src/components/navigationbar/components/actionButtons/index.js
@@ -1,47 +1,15 @@
-import { Box, IconButton } from "@chakra-ui/react";
-import { FaCartShopping, FaUser, FaBars } from "react-icons/fa6";
-import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
 
-const MotionIconButton = motion(IconButton); // Wrap Chakra's IconButton with motion
+import HamburgerButtonMobile from "./hamburgerButton";
+import CartButtonNav from "./cartButton";
+import UserButtonNav from "./userButton";
 
 const NavActionButtons = () => {
   return (
     <Box display="flex" alignItems="center">
-      <MotionIconButton
-        aria-label="Cart"
-        icon={<FaCartShopping />}
-        size="md"
-        mr={4}
-        display={{ base: "none", lg: "flex" }}
-        bg="transparent"
-        color="#56B280"
-        _hover={{ bg: "transparent" }}
-        whileHover={{ scale: 1.2, rotate: 10, transition: { duration: 0.2 } }} // Elegant lift and rotate effect
-        whileTap={{ scale: 0.9, transition: { duration: 0.1 } }} // Subtle shrink on click
-      />
-      <MotionIconButton
-        aria-label="User"
-        icon={<FaUser />}
-        size="md"
-        display={{ base: "none", lg: "flex" }}
-        bg="transparent"
-        color="#56B280"
-        _hover={{ bg: "transparent" }}
-        whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
-        whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-      />
-      <MotionIconButton
-        aria-label="Menu"
-        icon={<FaBars />}
-        size="md"
-        ml={4}
-        display={{ base: "flex", lg: "none" }}
-        bg="transparent"
-        color="#56B280"
-        _hover={{ bg: "transparent" }}
-        whileHover={{ scale: 1.2, rotate: 10, transition: { duration: 0.2 } }}
-        whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-      />
+      <CartButtonNav />
+      <UserButtonNav />
+      <HamburgerButtonMobile />
     </Box>
   );
 };
