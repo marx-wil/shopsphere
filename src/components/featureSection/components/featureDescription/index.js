76025687900da2506aed80c6b2fccf4af9714bf3
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-07 10:39:24 +0800

    feat: feature section feature description

---
+++ src/components/featureSection/components/featureDescription/index.js
@@ -0,0 +1,34 @@
+import { Stack, Text, Heading, useColorModeValue } from '@chakra-ui/react'
+import FeatureSectionIcon from '../featureIcon'
+
+const FeatureSectionDescription = () => {
+    return (
+        <Stack spacing={4}>
+            <Text
+                textTransform={"uppercase"}
+                color={"white"}
+                fontWeight={600}
+                fontSize={"sm"}
+                bg={useColorModeValue("#1D252C", "#397655")}
+                p={2}
+                alignSelf={"flex-start"}
+                rounded={"md"}
+            >
+                Our Journey.
+            </Text>
+            <Heading>Your garden.</Heading>
+            <Text color={"gray.500"} fontSize={"lg"}>
+                At ShopSphere, we believe that every plant has a story, just like
+                every home. Our passion for nature and sustainability drives us to
+                bring the beauty of the outdoors into your living space. From humble
+                beginnings to a carefully curated collection of plants, we are
+                dedicated to helping you nurture a greener, healthier life—one leaf
+                at a time. Join us on this journey to grow, learn, and thrive
+                together.
+            </Text>
+            <FeatureSectionIcon />
+        </Stack>
+    )
+}
+
+export default FeatureSectionDescription
