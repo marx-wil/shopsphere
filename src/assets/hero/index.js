e2d53e0bcf9a3d110f4e43baae690296ee8fe86d
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-07 13:25:11 +0800

    chore: reduced the size of image without reducing the quality for faster loading speed
    + removed unused images

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,10 +1,6 @@
 import MainHeroImage from "./front-view-parsnip.jpg";
-import GardenImage from "./gardening-concept-with-plants-apron.jpg";
-import HomeGardenArrangement from "./home-garden-arrangement-with-copy-space.jpg";
 import WhiteWoodBackgroundPlants from "./white-wooden-background-with-plants.jpg";
 export {
   MainHeroImage,
-  GardenImage,
-  HomeGardenArrangement,
   WhiteWoodBackgroundPlants,
 };
