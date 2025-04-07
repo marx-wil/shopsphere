208c10b26e432a0ac0dbe24e79d1ef9c9d24c3ea
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-07 13:31:51 +0800

    feat: import testimonial section

--- src/pages/client/default/index.js
+++ src/pages/client/default/index.js
@@ -1,10 +1,12 @@
 import FeaturesSection from "../../../components/featureSection";
 import HeroHeaderSection from "../../../components/heroHeaderSection";
+import TestimonialSection from "../../../components/testimonials";
 const ClientDefaultLayout = () => {
   return (
     <>
       <HeroHeaderSection />
       <FeaturesSection />
+      <TestimonialSection />
     </>
   );
 };
