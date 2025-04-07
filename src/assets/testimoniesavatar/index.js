78322e853bc8465bb52b8aea33533d862b7171d1
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-07 11:52:51 +0800

    feat: import and export testimonial avatar

---
+++ src/assets/testimoniesavatar/index.js
@@ -0,0 +1,4 @@
+import RockwellSouthAvatar from "./27470341_7294795.jpg"
+import JianneMiguelAvatar from "./27470349_7309670.jpg"
+import LeoMihoAvatar from "./27470334_7309681.jpg"
+export { RockwellSouthAvatar, JianneMiguelAvatar, LeoMihoAvatar };
