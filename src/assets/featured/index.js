d6b5f749fd320783f75a69312676e4280baad8c9
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-07 14:16:33 +0800

    chore: import and export featured photos

---
+++ src/assets/featured/index.js
@@ -0,0 +1,6 @@
+import Featured1 from "./2147768467.jpg"
+import Featured2 from "./2147768469.jpg"
+import Featured3 from "./2147768476.jpg"
+import Featured4 from "./2147768556.jpg"
+
+export { Featured1, Featured2, Featured3, Featured4 };
