ce19f9cea4bf34d09fd611bdbc9d336be6d2e92d
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-07 15:21:55 +0800

    chore: fixed type settings

--- src/components/featureSection/components/featureDescription/index.js
+++ src/components/featureSection/components/featureDescription/index.js
@@ -2,24 +2,26 @@ import { Stack, Text, Heading, useColorModeValue } from '@chakra-ui/react'
 import FeatureSectionIcon from '../featureIcon'
 
 const FeatureSectionDescription = () => {
+    const headingTextColor = useColorModeValue("#1D252C", "#FAF8F7")
+    const subtitleTextColor = useColorModeValue("#fff", "#fff")
     return (
         <Stack spacing={4}>
             <Text
                 textTransform={"uppercase"}
-                color={"white"}
-                fontWeight={600}
+                color={subtitleTextColor}
+                // fontWeight={600}
                 fontSize={"sm"}
                 bg={useColorModeValue("#1D252C", "#397655")}
                 p={2}
                 alignSelf={"flex-start"}
                 rounded={"md"}
-                className='poppins-regular'
+                className='poppins-bold'
             >
                 Our Journey.
             </Text>
             <Heading
-                className='poppins-regular'> Your garden.</Heading>
-            <Text color={"gray.500"} fontSize={"lg"} 
+                className='poppins-bold' textTransform={"uppercase"} color={headingTextColor}> Your garden.</Heading>
+            <Text color={"gray.500"} fontSize={"lg"}
                 className='poppins-regular'>
                 At ShopSphere, we believe that every plant has a story, just like
                 every home. Our passion for nature and sustainability drives us to
