7c7a01086587716e033b193dc183137380e00fcd
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-04-07 10:38:55 +0800

    feat: feature section icons

---
+++ src/components/featureSection/components/featureIcon/index.js
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+    Flex,
+    Text,
+    Stack,
+    StackDivider,
+    Icon,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import {
+    IoLogoBitcoin,
+    IoSearchSharp,
+} from "react-icons/io5";
+import { FaLeaf } from "react-icons/fa6";
+const Feature = ({ text, icon, iconBg }) => {
+    return (
+        <Stack direction={"row"} align={"center"}>
+            <Flex
+                w={8}
+                h={8}
+                align={"center"}
+                justify={"center"}
+                rounded={"full"}
+                bg={iconBg}
+            >
+                {icon}
+            </Flex>
+            <Text fontWeight={600}>{text}</Text>
+        </Stack>
+    );
+};
+const FeatureSectionIcon = () => {
+    return (
+        <Stack
+            spacing={4}
+            divider={
+                <StackDivider
+                    borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
+            }
+        >
+            <Feature
+                icon={<Icon as={FaLeaf} color={"#E16A67"} w={5} h={5} />}
+                iconBg={useColorModeValue("#964644", "#964644")}
+                text={"Curated Plants"}
+            />
+            <Feature
+                icon={<Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />}
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text={"Expert Care"}
+            />
+            <Feature
+                icon={<Icon as={IoSearchSharp} color={"#E5CFAA"} w={5} h={5} />}
+                iconBg={useColorModeValue("#988A71", "#988A71")}
+                text={"Sustainable Shipping"}
+            />
+        </Stack>
+    )
+};
+
+
+export default FeatureSectionIcon
