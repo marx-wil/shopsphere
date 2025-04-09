75886307af1c6e1a97daaae1f59e871575421b9a
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-09 09:47:53 +0800

    chore: replaced product-listing hero img

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,8 +1,8 @@
 import MainHeroImage from "./front-view-parsnip.jpg";
 import WhiteWoodBackgroundPlants from "./white-wooden-background-with-plants.jpg";
-import HeroHeaderImg from "./2149854861.jpg"
+import ProductListingHeroImg from "./12312321312.jpg"
 export {
   MainHeroImage,
   WhiteWoodBackgroundPlants,
-  HeroHeaderImg
+  ProductListingHeroImg
 };
