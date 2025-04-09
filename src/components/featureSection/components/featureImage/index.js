c018e9eb394eaf2d70b091479888cbb400474e57
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-09 13:41:45 +0800

    feat: image design

--- src/components/featureSection/components/featureImage/index.js
+++ src/components/featureSection/components/featureImage/index.js
@@ -1,19 +1,45 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { ManPlant } from "../../../../assets/images";
 
 const FeatureSectionImage = () => {
-    return (
-        <Flex>
-            <Image
-                rounded={"md"}
-                alt={"feature image"}
-                src={
-                    ManPlant
-                }
-                objectFit={"cover"}
-            />
-        </Flex>
-    )
-}
+  return (
+    <Flex>
+      <Box
+        width={"lg"} // set your desired image size
+        height={"lg"}
+        backgroundImage={`url(${ManPlant})`}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        rounded="md"
+        position="relative"
+        overflow="hidden"
+        sx={{
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            right: 0,
+            width: "8px",
+            height: "120px",
+            bg: useColorModeValue("#1D252C", "#397655"),
+            roundedTopRight: "md",
+            roundedBottomLeft: "lg",
+          },
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "8px",
+            height: "120px",
+            bg: useColorModeValue("#1D252C", "#397655"),
+            roundedTopRight: "md",
+            roundedBottomLeft: "lg",
+          },
+        }}
+      />
+    </Flex>
+  );
+};
 
-export default FeatureSectionImage
+export default FeatureSectionImage;
