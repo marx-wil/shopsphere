ec0845c28bdbdee6bde1417351ed587ed1b35d2e
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-09 09:44:37 +0800

    chore: import and export hero header image for product-listing

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,6 +1,8 @@
 import MainHeroImage from "./front-view-parsnip.jpg";
 import WhiteWoodBackgroundPlants from "./white-wooden-background-with-plants.jpg";
+import HeroHeaderImg from "./2149854861.jpg"
 export {
   MainHeroImage,
   WhiteWoodBackgroundPlants,
+  HeroHeaderImg
 };
