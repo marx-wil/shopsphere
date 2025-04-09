747f3ddef7e1a94f935730d67885e9b515fcecdb
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-09 09:50:27 +0800

    chore: import and export hero image 2 for product-listing

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,8 +1,10 @@
 import MainHeroImage from "./front-view-parsnip.jpg";
 import WhiteWoodBackgroundPlants from "./white-wooden-background-with-plants.jpg";
 import ProductListingHeroImg from "./12312321312.jpg"
+import ProductListingHeroImg2 from "./2149072119.jpg"
 export {
   MainHeroImage,
   WhiteWoodBackgroundPlants,
-  ProductListingHeroImg
+  ProductListingHeroImg,
+  ProductListingHeroImg2
 };
