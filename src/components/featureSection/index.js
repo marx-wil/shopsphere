4d68b78b75b142042116b4ec1d4b547f55705fea
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-10 09:48:13 +0800

    chore: fixed spacing and background colors

--- src/components/featureSection/index.js
+++ src/components/featureSection/index.js
@@ -1,19 +1,16 @@
-
-import {
-    Container,
-    SimpleGrid,
-} from "@chakra-ui/react";
+import { Container, SimpleGrid, Box, useColorModeValue } from "@chakra-ui/react";
 import FeatureSectionImage from "./components/featureImage";
 import FeatureSectionDescription from "./components/featureDescription";
 
-
 export default function FeaturesSection() {
-    return (
-        <Container maxW={"5xl"} py={12}>
-            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-                <FeatureSectionDescription />
-                <FeatureSectionImage />
-            </SimpleGrid>
-        </Container>
-    );
+  return (
+    <Box py={{base: 16, md: 24}} bg={useColorModeValue("#F7F8FA", "#0C1013")}>
+      <Container maxW={"5xl"} py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <FeatureSectionDescription />
+          <FeatureSectionImage />
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
