4b9021a6551b379be5a3e7e3288c344279ddda38
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-10 09:52:07 +0800

    refactor

--- src/components/featureSection/index.js
+++ src/components/featureSection/index.js
@@ -1,10 +1,15 @@
-import { Container, SimpleGrid, Box, useColorModeValue } from "@chakra-ui/react";
+import {
+  Container,
+  SimpleGrid,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import FeatureSectionImage from "./components/featureImage";
 import FeatureSectionDescription from "./components/featureDescription";
 
 export default function FeaturesSection() {
   return (
-    <Box py={{base: 16, md: 24}} bg={useColorModeValue("#F7F8FA", "#0C1013")}>
+    <Box py={{ base: 16, md: 24 }} bg={useColorModeValue("#F7F8FA", "#0C1013")}>
       <Container maxW={"5xl"} py={12}>
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
           <FeatureSectionDescription />
