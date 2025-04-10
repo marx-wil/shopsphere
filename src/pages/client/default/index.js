83a1a0b836937a00323d812d987ccd349b9e7480
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-10 09:48:30 +0800

    chore: added popular products removed testimonials

--- src/pages/client/default/index.js
+++ src/pages/client/default/index.js
@@ -1,13 +1,15 @@
 import FeaturedProducts from "../../../components/featuredProducts";
 import FeaturesSection from "../../../components/featureSection";
 import HeroHeaderSection from "../../../components/heroHeaderSection";
-import TestimonialSection from "../../../components/testimonials";
+import PopularProducts from "../../../components/popular";
+// import TestimonialSection from "../../../components/testimonials";
 const ClientDefaultLayout = () => {
   return (
     <>
       <HeroHeaderSection />
       <FeaturesSection />
       {/* <TestimonialSection /> */}
+      <PopularProducts />
       <FeaturedProducts />
     </>
   );
